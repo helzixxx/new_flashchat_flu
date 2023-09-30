b4fcf4a7e06caa06623b8f240da4226c6f607d51
Volha Zinkevich <o.zinkevich@icloud.com>
2023-09-30 22:14:08 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/new_flashchat_flu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.new_flashchat_flu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,94 @@
+import 'package:new_flashchat_flu/screens/login_screen.dart';
+import 'package:new_flashchat_flu/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import '../components/screen_button.dart';
+import 'package:new_flashchat_flu/constants.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  static const String id = '/welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      duration: Duration(seconds: 1),
+      vsync: this,
+    );
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white)
+        .animate(controller);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+      print(animation.value);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: animation.value,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
+                ),
+                AnimatedTextKit(
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Flash Chat',
+                      speed: const Duration(milliseconds: 100),
+                      textStyle: kLogoTextStyle,
+                    )
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 48.0,
+            ),
+            ScreenButton(
+              buttonText: 'Log In',
+              onPressed: () {
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+              color: Colors.lightBlueAccent,
+            ),
+            ScreenButton(
+              buttonText: 'Register',
+              onPressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+              color: Colors.blueAccent,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
